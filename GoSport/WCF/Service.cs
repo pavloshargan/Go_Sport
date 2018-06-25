91e128b777a1cdf95457de444bf2cc556dd4d459
pavloshargan <pavel.shargan@gmail.com>
2018-06-25 11:56:38 +0300

    FirstCommit

---
+++ GoSport/WCF/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using DAL;
+namespace WCF
+{
+    public class Service : IService
+    {
+        public void DoWork()
+        {
+
+        }
+    }
+}
---
+++ GoSport/WpfMaps/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.WPF;
+using Microsoft.Maps.MapControl.WPF.Design;
+namespace WpfMaps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void MapWithPushpins_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            Point mousePosition = e.GetPosition(this);
+            Location pinLocation = myMap.ViewportPointToLocation(mousePosition);
+            Pushpin pin = new Pushpin();
+            pin.Location = pinLocation;
+            myMap.Children.Add(pin);
+        }
+
+    }
+}
